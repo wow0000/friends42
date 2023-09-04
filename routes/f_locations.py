0be7aafcb79d0d2b213ec72976e571961305be2f
wow <github@wowow.cloud>
2023-09-04 03:39:21 +0200

    api42: add token monitoring

--- routes/f_locations.py
+++ routes/f_locations.py
@@ -8,7 +8,12 @@ app = Blueprint('locations', __name__, template_folder='templates')
 
 @app.route('/locations/<token>/<int:campus>')
 def update_locs2(token, campus):
-	if token == config.update_key:
-		locs(campus)
-		return '', 200
-	return '', 400
+	if token != config.update_key:
+		return 'Bad token', 400
+	locs(campus)
+	return 'OK', 200
+
+
+@app.route('/locations/monitoring/<token>')
+def monitoring(token):
+	return '', 200 if api.get_token() else 500
