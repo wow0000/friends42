f9e47d287b9d7c51229040f05027ceafe37d1c73
wow <github@wowow.cloud>
2024-02-18 13:11:20 +0100

    users: fix correct campus id for 42 network/centrale

--- routes/api_helpers.py
+++ routes/api_helpers.py
@@ -1,14 +1,20 @@
 def find_correct_campus(elem):
-	if 'campus_users' in elem:
-		for campus_data in elem['campus_users']:
-			if campus_data['is_primary']:
-				return campus_data['campus_id']
-	if 'user' in elem and 'campus_users' in elem['user']:
-		for campus_data in elem['user']['campus_users']:
-			if campus_data['is_primary']:
-				return campus_data['campus_id']
-	if 'campus_id' in elem:
-		return elem['campus_id']
-	if 'campus' in elem['user'] and len(elem['user']['campus']) > 0:
-		return elem['user']['campus'][0]['id']
-	return 1
+	def get_campus():
+		if 'campus_users' in elem:
+			for campus_data in elem['campus_users']:
+				if campus_data['is_primary']:
+					return campus_data['campus_id']
+		if 'user' in elem and 'campus_users' in elem['user']:
+			for campus_data in elem['user']['campus_users']:
+				if campus_data['is_primary']:
+					return campus_data['campus_id']
+		if 'campus_id' in elem:
+			return elem['campus_id']
+		if 'campus' in elem['user'] and len(elem['user']['campus']) > 0:
+			return elem['user']['campus'][0]['id']
+		return 1
+	campus = get_campus()
+	# 42 network, 42 centrale
+	if campus == 42 or campus == 54:
+		return 1
+	return campus
